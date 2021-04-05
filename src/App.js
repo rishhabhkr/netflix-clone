6382ba0e6251908ee6b9dea1a632ac99c828c523
Rishabh Kumar <riskumar7@publicisgroupe.net>
2021-04-05 23:37:33 +0530

    added Row.js and Row.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react'
-
+import React from "react";
+import './App.css'
+import Row from "./Components/Row";
+import requests from "./requests"
 function App() {
   return (
     <div className="App">
-
-    <h1>Hello</h1>
-
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Action" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
