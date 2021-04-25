276d3cff9a978108f70134b7c0c1c1e5b64b095f
Rishabh Kumar <riskumar7@publicisgroupe.net>
2021-04-25 19:17:47 +0530

    added banner and navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import './App.css'
 import Row from "./Components/Row";
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
+import Banner from './Components/Banner'
 import requests from "./requests"
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <Banner />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
       <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} />
@@ -13,6 +18,7 @@ function App() {
       <Row title="Horror" fetchUrl={requests.fetchHorrorMovies} />
       <Row title="Romance" fetchUrl={requests.fetchRomanceMovies} />
       <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+      <Footer />
     </div>
   );
 }
--- src/Components/Row.js
+++ src/Components/Row.js
@@ -18,7 +18,7 @@ function Row({ title, fetchUrl }) {
 
   return (
     <div className="row">
-      <h1>{title}</h1>
+      <h2>{title}</h2>
 
       {/* poster container */}
 
