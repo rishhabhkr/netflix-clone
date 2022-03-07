daf608a49d3c5cebbcc8787a6b803015c3e20a18
Rishabh Kumar <rishu.kumr@gmail.com>
2022-03-07 16:19:47 +0530

    added trailer pop ups removed footer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from "react";
+import React from 'react'
 import './App.css'
-import Row from "./Components/Row";
-import Navbar from './Components/Navbar';
-import Footer from './Components/Footer';
-import Banner from './Components/Banner';
-import requests from "./requests"
+import Row from './Components/Row'
+import Navbar from './Components/Navbar'
+import Banner from './Components/Banner'
+import requests from './requests'
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Navbar />
       <Banner />
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
-      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
-      <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} />
-      <Row title="Action" fetchUrl={requests.fetchActionMovies} />
-      <Row title="Comedy" fetchUrl={requests.fetchComedyMovies} />
-      <Row title="Horror" fetchUrl={requests.fetchHorrorMovies} />
-      <Row title="Romance" fetchUrl={requests.fetchRomanceMovies} />
-      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
-      <Footer />
+      <Row
+        title='Netflix Originals'
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true}
+      />
+      <Row title='Trending Now' fetchUrl={requests.fetchTrending} />
+      <Row title='Top Rated' fetchUrl={requests.fetchTopRated} />
+      <Row title='Action' fetchUrl={requests.fetchActionMovies} />
+      <Row title='Comedy' fetchUrl={requests.fetchComedyMovies} />
+      <Row title='Horror' fetchUrl={requests.fetchHorrorMovies} />
+      <Row title='Romance' fetchUrl={requests.fetchRomanceMovies} />
+      <Row title='Documentaries' fetchUrl={requests.fetchDocumentaries} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
